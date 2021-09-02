6aa34ff59778d77aa6bf4fee2b204f93f3d82416
Randy DePriest <jimrdepr@ut.utm.edu>
2021-09-02 16:25:27 -0500

    Added very basic hit detection

--- New Unity Project/Assets/Player_Attack.cs
+++ New Unity Project/Assets/Player_Attack.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class Player_Attack : MonoBehaviour
 {
+    RaycastHit2D hit;
     float cooldown = 0;
-    float range = 5;
+    float range = 2;
+    Vector2 direction;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,11 +17,19 @@ public class Player_Attack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKey("w"))
+            direction = Vector2.up;
+        if(Input.GetKey("a"))
+            direction = Vector2.left;
+        if(Input.GetKey("s"))
+            direction = Vector2.down;
+        if(Input.GetKey("d"))
+            direction = Vector2.right;
         if(cooldown <= 0)
         {
             if(Input.GetKeyDown("space"))
             {
-                cooldown = 5;
+                cooldown = 1;
                 Attack();
             }
 
@@ -30,7 +40,13 @@ public class Player_Attack : MonoBehaviour
     }
     void Attack()
     {
-        print(cooldown);
-        print("attacking");
+        int layerMask = 1 << 8;
+        layerMask = ~layerMask;
+        print(layerMask);
+        hit = Physics2D.Raycast(transform.position, direction, range, layerMask);
+        if(hit.collider != null)
+            print(hit.collider.gameObject.name);
+        else
+            print("missed");
     }
 }
